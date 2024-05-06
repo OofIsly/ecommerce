f7889f4ddd1304d4d0a3cf3baf4cb3ec1f97411f
OofIsly <isly.yamete@gmail.com>
2024-05-06 19:40:41 +0800

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+      <b> Bosante & Liabado</b>
+    </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+	<?php include 'includes/navbar.php'; ?>
+	 
+	  <div class="content-wrapper">
+	    <div class="container">
+
+	        		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+		                <ol class="carousel-indicators">
+		                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+		                  <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
+		                  <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
+		                </ol>
+		                <div class="carousel-inner">
+		                  <div class="item active">
+		                    <img src="images/banner1.jpg" alt="First slide">
+		                  </div>
+		                  <div class="item">
+		                    <img src="images/banner2.jpg" alt="Second slide">
+		                  </div>
+		                  <div class="item">
+		                    <img src="images/banner3.jpg" alt="Third slide">
+		                  </div>
+		                </div>
+		                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+		                  <span class="fa fa-angle-left"></span>
+		                </a>
+		                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+		                  <span class="fa fa-angle-right"></span>
+		                </a>
+		            </div>
+		            
+	        	<div class="col-sm-3">
+	        		<?php include 'includes/sidebar.php'; ?>
+	        	</div>
+	        </div>
+	      </section>
+	     
+	    </div>
+	  </div>
+  
+  	<?php include 'includes/footer.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+
+
+</body>
+</html>
